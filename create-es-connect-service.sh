827aa2fee80e6b13ee714acce66df5a685adb139
blank <bakareemmanuel98@outlook.com>
2018-06-04 18:39:21 +0100

    Added kafka dependency to schema-registry and es sink connector request using kafka rest

---
+++ create-es-connect-service.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# Create sink service for ES using Kafka Connect endpoint
+curl -X POST http://kafka-connect:8083/connectors/ES-Sink -H "Content-Type: application/json" -d '{
+  "name": "gritServer",
+  "config": {
+    "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
+    "tasks.max": "1",
+    "topics": "gritServer",
+    "key.ignore": "true",
+    "schema.ignore": "true",
+    "connection.url": "http://elasticsearch:9200",
+    "type.name": "json",
+    "name": "ES-Sink"
+  }
+}';
